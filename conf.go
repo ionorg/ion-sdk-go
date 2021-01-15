aacb9d099fca5a95fbd991ada0b0dbe37ceae436
adwpc <adwpc@hotmail.com>
2021-01-15 15:41:44 +0800

    update

--- conf.go
+++ conf.go
@@ -1,16 +1,21 @@
 package engine
 
-import log "github.com/pion/ion-log"
+import (
+	log "github.com/pion/ion-log"
+	"github.com/pion/webrtc/v3"
+)
 
 type ICEConf struct {
-	URLs       []string `mapstructure:"urls"`
-	Username   string   `mapstructure:"username"`
-	Credential string   `mapstructure:"credential"`
+	URLs           []string                 `mapstructure:"urls"`
+	Username       string                   `mapstructure:"username"`
+	Credential     string                   `mapstructure:"credential"`
+	CredentialType webrtc.ICECredentialType `mapstructure:"credentialtype"`
 }
 
 type WebRTCConf struct {
 	ICEPortRange []uint16  `mapstructure:"portrange"`
 	ICEServers   []ICEConf `mapstructure:"iceserver"`
+	ICELite      bool      `mapstructure:"icelite"`
 }
 
 // Config for base AVP
