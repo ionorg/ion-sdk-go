da4b63bf784e7832bf63f39fb8eb08d1ecd3c7bb
Kevin De Jong <kevin@tinymile.ai>
2021-02-02 17:05:47 -0500

    Add OnError handler to Signal and Client

--- client.go
+++ client.go
@@ -34,6 +34,7 @@ type Client struct {
 	//export to user
 	OnTrack       func(track *webrtc.TrackRemote, receiver *webrtc.RTPReceiver)
 	OnDataChannel func(*webrtc.DataChannel)
+	OnError       func(error)
 
 	producer *WebMProducer
 	recvByte int
@@ -61,6 +62,11 @@ func NewClient(addr string, cfg WebRTCTransportConfig) *Client {
 	c.signal.OnNegotiate = c.Negotiate
 	c.signal.OnTrickle = c.Trickle
 	c.signal.OnSetRemoteSDP = c.SetRemoteSDP
+	c.signal.OnError = func(err error) {
+		if c.OnError != nil {
+			c.OnError(err)
+		}
+	}
 
 	// this will be called when pub add/remove/replace track, but pion never triger, why?
 	// c.pub.pc.OnNegotiationNeeded(c.OnNegotiationNeeded)
