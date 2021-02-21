396e35ded0e631ba3399c3b542a3b2843d399684
Leeward Bound <l@lwb.co>
2021-02-21 10:10:52 -0800

    fix: my code was missing -_-

--- example/join-from-mediadevice/main.go
+++ example/join-from-mediadevice/main.go
@@ -3,44 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strings"
 
 	log "github.com/pion/ion-log"
 	sdk "github.com/pion/ion-sdk-go"
 	"github.com/pion/webrtc/v3"
-	"github.com/pion/webrtc/v3/pkg/media"
-	"github.com/pion/webrtc/v3/pkg/media/ivfwriter"
-	"github.com/pion/webrtc/v3/pkg/media/oggwriter"
+	"github.com/pion/mediadevices"
+	"github.com/pion/mediadevices/pkg/codec/vpx"
+	"github.com/pion/mediadevices/pkg/frame"
+	"github.com/pion/mediadevices/pkg/prop"
+
+	// Note: If you don't have a camera or microphone or your adapters are not supported,
+	//       you can always swap your adapters with our dummy adapters below.
+	// _ "github.com/pion/mediadevices/pkg/driver/videotest"
+	// _ "github.com/pion/mediadevices/pkg/driver/audiotest"
+	_ "github.com/pion/mediadevices/pkg/driver/camera"     // This is required to register camera adapter
+	_ "github.com/pion/mediadevices/pkg/driver/microphone" // This is required to register microphone adapter
+
 )
-
-func saveToDisk(track *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
-	codec := track.Codec()
-	var fileWriter media.Writer
-	var err error
-	if strings.EqualFold(codec.MimeType, webrtc.MimeTypeOpus) {
-		log.Infof("Got Opus track, saving to disk as ogg (48 kHz, 2 channels)")
-		fileWriter, err = oggwriter.New(fmt.Sprintf("%d_%d.ogg", codec.PayloadType, track.SSRC()), 48000, 2)
-	} else if strings.EqualFold(codec.MimeType, webrtc.MimeTypeVP8) {
-		log.Infof("Got VP8 track, saving to disk as ivf")
-		fileWriter, err = ivfwriter.New(fmt.Sprintf("%d_%d.ivf", codec.PayloadType, track.SSRC()))
-	}
-
-	if err != nil {
-		log.Errorf("err=%v", err)
-		fileWriter.Close()
-		return
-	}
-
-	for {
-		rtpPacket, _, err := track.ReadRTP()
-		if err != nil {
-			panic(err)
-		}
-		if err := fileWriter.WriteRTP(rtpPacket); err != nil {
-			panic(err)
-		}
-	}
-}
 
 func main() {
 	// init log
@@ -49,8 +28,7 @@ func main() {
 	log.Init("debug", fixByFile, fixByFunc)
 
 	// parse flag
-	var session, addr, file string
-	flag.StringVar(&file, "file", "./file.webm", "Path to the file media")
+	var session, addr string
 	flag.StringVar(&addr, "addr", "localhost:50051", "Ion-sfu grpc addr")
 	flag.StringVar(&session, "session", "test room", "join session name")
 	flag.Parse()
@@ -78,21 +56,52 @@ func main() {
 	// get a client from engine
 	c := e.AddClient(addr, session, "client id")
 
-	// subscribe rtp from sessoin
-	// comment this if you don't need save to file
-	c.OnTrack = saveToDisk
-
 	// client join a session
 	err := c.Join(session)
 
-	// publish file to session if needed
-	if err == nil && file != "" {
-		err = c.PublishWebm(file)
-		if err != nil {
-			log.Errorf("err=%v", err)
-		}
-	} else {
+	if err != nil {
 		log.Errorf("err=%v", err)
+		panic(err)
 	}
+
+
+	vpxParams, err := vpx.NewVP8Params()
+	if err != nil {
+		panic(err)
+	}
+	vpxParams.BitRate = 500_000 // 500kbps
+
+	codecSelector := mediadevices.NewCodecSelector(
+		mediadevices.WithVideoEncoders(&vpxParams),
+	)
+
+	fmt.Println(mediadevices.EnumerateDevices())
+
+	s, err := mediadevices.GetUserMedia(mediadevices.MediaStreamConstraints{
+		Video: func(c *mediadevices.MediaTrackConstraints) {
+			c.FrameFormat = prop.FrameFormat(frame.FormatYUY2)
+			c.Width = prop.Int(640)
+			c.Height = prop.Int(480)
+		},
+		Codec: codecSelector,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, track := range s.GetTracks() {
+		track.OnEnded(func(err error) {
+			fmt.Printf("Track (ID: %s) ended with error: %v\n",
+				track.ID(), err)
+		})
+		_, err = c.Publish(track)
+		if err != nil {
+			panic(err)
+		} else {
+			break // only publish first track, thanks
+		}
+	}
+
 	select {}
 }
