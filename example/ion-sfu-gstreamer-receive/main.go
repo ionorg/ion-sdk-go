8c36dca5ceb84f4cfbf224f2cb9d3c49a69b73ee
adwpc <adwpc@hotmail.com>
2021-03-14 15:17:34 +0800

    rename internal to pkg

--- example/ion-sfu-gstreamer-receive/main.go
+++ example/ion-sfu-gstreamer-receive/main.go
@@ -9,7 +9,7 @@ import (
 
 	log "github.com/pion/ion-log"
 	sdk "github.com/pion/ion-sdk-go"
-	gst "github.com/pion/ion-sdk-go/internal/gstreamer-sink"
+	gst "github.com/pion/ion-sdk-go/pkg/gstreamer-sink"
 	"github.com/pion/rtcp"
 	"github.com/pion/webrtc/v3"
 )
